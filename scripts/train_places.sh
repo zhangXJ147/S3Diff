5a1bf1b362f7d345e9ae61f69c82e997ed853245
zhangXJ147 <89435311+zhangXJ147@users.noreply.github.com>
2026-07-23 10:38:29 +0800

    Update train_places.sh

--- scripts/train_places.sh
+++ scripts/train_places.sh
@@ -5,6 +5,6 @@ for ((i=1;i<=50;i++))
 
 do
 
-CUDA_VISIBLE_DEVICES=0 python main.py --task image --image_name places50/$i.jpg --run_name train_j8
+CUDA_VISIBLE_DEVICES=0 python main.py --task image --image_name places50/$i.jpg --run_name train_places50
 
 done
