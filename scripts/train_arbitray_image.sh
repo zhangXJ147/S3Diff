d436db4ea08220f2242c4476e0a4e53e7fe3a67e
zhangXJ147 <89435311+zhangXJ147@users.noreply.github.com>
2026-08-29 10:32:47 +0800

    Update train_arbitray_image.sh

--- scripts/train_arbitray_image.sh
+++ scripts/train_arbitray_image.sh
@@ -2,4 +2,3 @@
 export NCCL_P2P_DISABLE=1
 
 CUDA_VISIBLE_DEVICES=0 python main.py --task image --image_name balloons.png --run_name version_1
-#CUDA_VISIBLE_DEVICES=0 python main.py --task image --image_name lsun50/42.jpg --run_name train_lsun50_42
